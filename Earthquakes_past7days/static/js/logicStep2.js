a0f2ba066d68a787c81cefd0e81de6ce7695b4ed
SecretDoves3000 <theofner1@gmail.com>
2023-02-09 16:57:47 -0500

    update

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -44,7 +44,7 @@ function styleInfo(feature) {
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: getColor(feature.properties.mag),
+    fillColor: "#ffae42",
     color: "#000000",
     radius: getRadius(feature.properties.mag),
     stroke: true,
@@ -52,8 +52,26 @@ function styleInfo(feature) {
   };
 }
 
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+  L.geoJSON(data, {
+
+  // We turn each feature into a circleMarker on the map.
+  pointToLayer: function(feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng);
+        },
+      // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo
+  }).addTo(map);
 });
